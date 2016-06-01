27d9579c30822b9ba8951c945ee50d63ebe0747c
Pierre Oucif <pierre.oucif@gadz.org>
2016-06-01 11:15:19 +0200

    Ajout d'icones

--- scripts/page1Controller.js
+++ scripts/page1Controller.js
@@ -21,5 +21,36 @@
             self.selected=page1;
         }
     }
+    
+    function makeContact(selectedPage1) {
+
+        $mdBottomSheet.show({
+          controllerAs  : "vm",
+          templateUrl   : './view/contactSheet.html',
+          controller    : [ '$mdBottomSheet', ContactSheetController],
+          parent        : angular.element(document.getElementById('content'))
+        }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * Page1 ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.page1 = selectedPage1;
+          this.items = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'images/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'images/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'images/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'images/hangouts.svg'}
+          ];
+          this.contactPage1 = function(action) {
+            // The actually contact process has not been implemented...
+            // so just hide the bottomSheet
+
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
 
 })();
